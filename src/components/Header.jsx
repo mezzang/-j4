ab0081bb5b61a260e5802d425bb7e579f7e46ca4
mejin <wls4482@gmail.com>
2024-11-22 17:10:06 +0900

    style(header): css 적용

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,18 +1,69 @@
-// components/Header.js
 import React from "react";
+import styled from "styled-components";
+
+const Title = styled.div`
+  position: absolute;
+  width: 1002px;
+  height: 191px;
+  top: 277px;
+  left: 244px;
+`;
+
+const Image = styled.img`
+  position: absolute;
+  width: 121px;
+  height: 150px;
+  top: 26px;
+  left: 146px;
+`;
+
+const OverlapGroup = styled.div`
+  position: absolute;
+  width: 461px;
+  height: 117px;
+  top: 23px;
+  left: 277px;
+`;
+
+const TextWrapper24 = styled.div`
+  width: 461px;
+  -webkit-text-stroke: 2px #a64040;
+  font-family: "Jersey 20-Regular", Helvetica, sans-serif;
+  font-weight: 400;
+  color: #f9f235;
+  font-size: 105px;
+  white-space: nowrap;
+`;
+
+const TextWrapper25 = styled.div`
+  width: 62px;
+  font-family: "Jaro-Regular", Helvetica, sans-serif;
+  font-weight: 400;
+  color: #de0404;
+  font-size: 30px;
+  white-space: nowrap;
+`;
+
+const TextWrapper26 = styled.div`
+  width: 211px;
+  font-family: "Ink Free-Regular", Helvetica, sans-serif;
+  font-weight: 400;
+  color: #fd2323;
+  font-size: 25px;
+`;
 
 function Header() {
   return (
-    <div className="title">
-      <img className="image" src="image.png" alt="Logo" />
-      <div className="overlap-group">
-        <div className="text-wrapper-24">Seminar.zip</div>
+    <Title>
+      <Image src="호반우.png" alt="호반우" />
+      <OverlapGroup>
+        <TextWrapper24>Seminar.zip</TextWrapper24>
         <div className="KNU-MARK">
-          <div className="text-wrapper-25">KNU</div>
+          <TextWrapper25>KNU</TextWrapper25>
         </div>
-      </div>
-      <div className="text-wrapper-26">for knu_it students</div>
-    </div>
+      </OverlapGroup>
+      <TextWrapper26>for knu_it students</TextWrapper26>
+    </Title>
   );
 }
 
